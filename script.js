98680fba4f1b134143d0c6beeb215d05354005a7
Gaspy <mpgasbarro@gmail.com>
2020-07-10 14:43:52 -0400

    added and stylized  modal for how to play game, made minor edit to README

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello");
+// Grabbing elements for How to Play
+const openBtn = document.getElementById('howToPlayModal');
+const modal = document.getElementById('modal');
+const close = document.getElementById('close');
+
+//Functions for How To Play
+const openModal = () => {
+	modal.style.display = 'block';
+};
+
+const closeModal = () => {
+	modal.style.display = 'none';
+};
+
+//Event Listeners for How to Play
+openBtn.addEventListener('click', openModal);
+
+close.addEventListener('click', closeModal);
