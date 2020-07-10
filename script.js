e9d966946055ae1a8e89c513865b13a851f3bcea
Gaspy <mpgasbarro@gmail.com>
2020-07-10 19:34:44 -0400

    played around with some visual aspects

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// Grabbing elements for How to Play
+// Grabbing elements for How to Play Button
 const openBtn = document.getElementById('howToPlayModal');
 const modal = document.getElementById('modal');
 const close = document.getElementById('close');
 
-//Functions for How To Play
+//Functions for How To Play Button
 const openModal = () => {
 	modal.style.display = 'block';
 };
-
 const closeModal = () => {
 	modal.style.display = 'none';
 };
 
-//Event Listeners for How to Play
+//Event Listeners for How to Play Button
 openBtn.addEventListener('click', openModal);
 
 close.addEventListener('click', closeModal);
