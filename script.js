f28720b1e0b48d83010456abeb50bc6f3504aa72
Gaspy <mpgasbarro@gmail.com>
2020-07-13 13:49:46 -0400

    updated letter guess button function to recognize letter input from wordBank

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const openBtn = document.getElementById('howToPlayModal');
 const modal = document.getElementById('modal');
 const close = document.getElementById('close');
+// Element for hidden word
 const wordPop = document.getElementById('wordPop');
 //Functions for How To Play Button
 const openModal = () => {
@@ -16,13 +17,44 @@ openBtn.addEventListener('click', openModal);
 
 close.addEventListener('click', closeModal);
 
+let wordBank = ['bike'];
+// function to breakdown array (wordBank) into single characters
+
+// Randomizes the selection from the wordBank
+const randomWordChoice = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+// For loop to randomize word and add it to the page
+for (let i = 0; i < randomWordChoice.length; i++) {
+	letter = document.createElement('span');
+	letter.innerText = '_____';
+	letter.classList.add('letter');
+	wordPop.appendChild(letter);
+	console.log(randomWordChoice);
+}
+
 // Grabbing Element for letter guess button
 let enterBtn = document.querySelector('#enterGuess');
 // Function for letter guess button
 function playerGuess(event) {
 	event.preventDefault();
+	let randomWordChoiceLetters = randomWordChoice.split('');
+	console.log(randomWordChoiceLetters[0]);
 	let userInput = document.querySelector('.input').value;
-	if (userInput === 'e') {
+	if (userInput === randomWordChoiceLetters[0]) {
+		console.log('hello there mike');
+	} else if (userInput === randomWordChoiceLetters[1]) {
+		console.log('hello there mike');
+	} else if (userInput === randomWordChoiceLetters[2]) {
+		console.log('hello there mike');
+	} else if (userInput === randomWordChoiceLetters[3]) {
+		console.log('hello there mike');
+	} else if (userInput === randomWordChoiceLetters[4]) {
+		console.log('hello there mike');
+	} else if (userInput === randomWordChoiceLetters[5]) {
+		console.log('hello there mike');
+	} else if (userInput === randomWordChoiceLetters[6]) {
+		console.log('hello there mike');
+	} else if (userInput === randomWordChoiceLetters[7]) {
 		console.log('hello there mike');
 	} else {
 		console.log('hello there bob');
@@ -30,38 +62,3 @@ function playerGuess(event) {
 }
 // Event listener for Letter Guess button
 enterBtn.addEventListener('click', playerGuess);
-//
-//
-//
-//
-//
-let wordBank = ['bike', 'dog', 'cat', 'bed', 'house'];
-// function to breakdown array (wordBank) into single characters
-
-console.log(wordBank);
-const random = wordBank[Math.floor(Math.random() * wordBank.length)];
-
-console.log(random);
-
-for (let i = 0; i < random.length; i++) {
-	const letter = document.createElement('span');
-	letter.innerText = '_';
-	letter.classList.add('letter');
-	wordPop.appendChild(letter);
-	console.log(wordBank);
-}
-
-// const words = {
-// 	word1: "Bike",
-// 	word2: "Dog",
-// 	word3: "cat",
-// 	wordGuess: function playerGuess (guess) {
-// 		if (guess === this.word1 || guess === this.word2) {
-// 			let splitWord = guess.split("")
-// 			console.log(splitWord);
-// 		}
-
-// 	}
-
-// };
-// words.wordGuess("Dog");
